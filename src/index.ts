95b119f168f33f47c5dadc7696782fbaac765e4d
x44annie <2006basstester@gmail.com>
2025-08-19 00:46:55 +0200

    Minor fix.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import 'dotenv/config'
 import { client } from '@/shared/client'
 import { registerAllEvents } from '@/events'
 import { validateEnvVars } from '@/shared/config/env-check'
+import * as process from 'node:process'
+const tokem = process.env.TOKEN;
 
 validateEnvVars()
 registerAllEvents()
