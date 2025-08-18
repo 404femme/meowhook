6593e30bcbf53c74485ee827bc18fd14a9bfb0e4
x44annie <2006basstester@gmail.com>
2025-08-19 01:02:21 +0200

    Fix: test fix.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { client } from '@/shared/client'
 import { registerAllEvents } from '@/events'
 import { validateEnvVars } from '@/shared/config/env-check'
 import * as process from 'node:process'
-const tokem = process.env.TOKEN;
 
 validateEnvVars()
 registerAllEvents()
